df8a28b9fd9e1eb95d51c22eca9c2072383d581f
v-akumar <animesh42001@gmail.com>
2019-02-20 14:33:31 -0500

    simple app for automation exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="items" id="items">
+          <div className="items-list">
+              <div className="label" id="lbl_val_1">Value 1</div>
+              <div className="amount" id="txt_val_1">$122,365.24</div>
+              <div className="label" id="lbl_val_2">Value 2</div>
+              <div className="amount" id="txt_val_2">$599.00</div>
+              <div className="label" id="lbl_val_3">Value 3</div>
+              <div className="amount" id="txt_val_3">$850,139.99</div>
+              <div className="label" id="lbl_val_4">Value 4</div>
+              <div className="amount" id="txt_val_4">$23,329.50</div>
+              <div className="label" id="lbl_val_5">Value 5</div>
+              <div className="amount" id="txt_val_5">$566.27</div>
+              <div className="label" id="lbl_ttl_val">Total Balance</div>
+              <div className="amount" id="txt_ttl_val">$1,000,000.00</div>
+          </div>
       </div>
     );
   }
---
+++ src/staticexercise.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+class staticexercise extends Component {
+  render() {
+    return (
+      <div className="totalBalance">
+        <div className="header">
+          <form>
+            <label>
+               <input type="label" name="label" />
+               <input type="value" name="value" />
+            </label>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default staticexercise
